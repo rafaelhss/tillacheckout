c9799b50592805e63feef6587c8092bec7443af3
frubbs <rafaelhss@gmail.com>
2016-09-01 18:00:54 -0300

    v2

--- src/main/resources/static/js/tillaapp.js
+++ src/main/resources/static/js/tillaapp.js
@@ -5,8 +5,8 @@ app.service('TillaConfig',function(){
     this.apiUrl = "http://tillacheckout.herokuapp.com/api";
     this.adminUrl = "http://tillacheckout.herokuapp.com/admin";
 
-    this.apiUrl = "http://localhost:8080/api";
-    this.adminUrl = "http://localhost:8080/admin";
+    //this.apiUrl = "http://localhost:8080/api";
+    //this.adminUrl = "http://localhost:8080/admin";
 });
 
 
